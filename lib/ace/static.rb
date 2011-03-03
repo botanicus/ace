92d1eb155350cb9526e7a6fc16c24f19d0533860
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-03 16:24:25 +0000

    Use rule Ace::Static(output_path), file if you need to specify output_path.
    Example: rule Ace::Static("output/index.html"), "index.html.haml"

--- lib/ace/static.rb
+++ lib/ace/static.rb
@@ -3,7 +3,16 @@
 require "ace/filters/template"
 
 # rule Ace::Static, "about.html.haml"
+# rule Ace::Static("output/index.html"), "index.html.haml"
 module Ace
+  def self.Static(output_path)
+    Class.new(Static) do
+      define_method(:output_path) do
+        output_path
+      end
+    end
+  end
+
   class Static < Item
     before Ace::TemplateFilter
   end
