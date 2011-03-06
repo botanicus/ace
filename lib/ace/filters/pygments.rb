e3941ba8e539c01703f08c504cb30d357be24063
hotovson <pepe.hotovson@gmail.com>
2011-03-06 20:14:08 +0100

    Added missing Ace sandbox in Ace::PygmentsFilter.

--- lib/ace/filters/pygments.rb
+++ lib/ace/filters/pygments.rb
@@ -4,16 +4,18 @@ require "ace/filters"
 require "nokogiri"
 require "albino"
 
-class PygmentsFilter < Filter
-  def call(item, content)
-    puts 'PygmentsFilter ************************'
-    doc = Nokogiri::HTML(content)
-    doc.css('pre').each do |pre|
-      puts "\nWARNING: '#{item.original_path}' - element <pre> not contains attribute 'lang'\n" if pre['lang'].nil?
-      puts "\nWARNING: '#{item.original_path}' - attribute 'lang' not contains any value\n" if  !pre['lang'].nil? && pre['lang'].empty?
-      puts "*** Syntax highlight using '#{pre['lang']}' lexer" unless pre['lang'].nil? || pre['lang'].empty?
-      pre.replace Albino.colorize(pre.content, pre['lang']) unless pre['lang'].nil? || pre['lang'].empty?
+module Ace
+  class PygmentsFilter < Filter
+    def call(item, content)
+      puts 'PygmentsFilter ************************'
+      doc = Nokogiri::HTML(content)
+      doc.css('pre').each do |pre|
+        puts "\nWARNING: '#{item.original_path}' - element <pre> not contains attribute 'lang'\n" if pre['lang'].nil?
+        puts "\nWARNING: '#{item.original_path}' - attribute 'lang' not contains any value\n" if  !pre['lang'].nil? && pre['lang'].empty?
+        puts "*** Syntax highlight using '#{pre['lang']}' lexer" unless pre['lang'].nil? || pre['lang'].empty?
+        pre.replace Albino.colorize(pre.content, pre['lang']) unless pre['lang'].nil? || pre['lang'].empty?
+      end
+      doc
     end
-    doc
   end
 end
