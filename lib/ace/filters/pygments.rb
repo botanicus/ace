b6e582e7b04e7cefb66d757f1fd3a1ca7c3b64eb
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-03-07 14:22:08 +0000

    Refactoring, less warnings.

--- lib/ace/filters/pygments.rb
+++ lib/ace/filters/pygments.rb
@@ -7,15 +7,15 @@ require "albino"
 module Ace
   class PygmentsFilter < Filter
     def call(item, content)
-      puts 'PygmentsFilter ************************'
       doc = Nokogiri::HTML(content)
-      doc.css('pre').each do |pre|
-        puts "\nWARNING: '#{item.original_path}' - element <pre> not contains attribute 'lang'\n" if pre['lang'].nil?
-        puts "\nWARNING: '#{item.original_path}' - attribute 'lang' not contains any value\n" if  !pre['lang'].nil? && pre['lang'].empty?
-        puts "*** Syntax highlight using '#{pre['lang']}' lexer" unless pre['lang'].nil? || pre['lang'].empty?
-        pre.replace Albino.colorize(pre.content, pre['lang']) unless pre['lang'].nil? || pre['lang'].empty?
+      doc.css("pre[lang]").each do |pre|
+        unless pre['lang'].nil? || pre['lang'].empty?
+          # Set $VERBOSE to nil if you don't want to see this message.
+          warn "~ Syntax highlight in '#{item.original_path}' using '#{pre['lang']}' lexer."
+          pre.replace Albino.colorize(pre.content, pre['lang'])
+        end
       end
-      doc
+      doc.to_s
     end
   end
 end
