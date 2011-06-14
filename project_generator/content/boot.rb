0889e03f44c3185683ed295db6a72e634ee2298e
Jakub Stastny aka botanicus <stastny@101ideas.cz>
2011-06-14 14:17:32 +0100

    Ups, no helpers.

--- project_generator/content/boot.rb
+++ project_generator/content/boot.rb
@@ -11,8 +11,6 @@ require "bundler/setup"
 # require "pupu/adapters/ace"
 # Pupu.media_prefix = "/assets"
 
-require "helpers"
-
 # Load the app.
 Dir["app/**/*.rb"].each do |file|
   puts "~ Loading #{file}"
